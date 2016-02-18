f222c5bee5e60f251289705a587f54863ad777ac
youchra <nemmo@gmx.de>
2016-02-18 19:45:09 +0100

    building dist

--- clTable/dist/clTable.js
+++ clTable/dist/clTable.js
@@ -1,1 +1,36 @@
-var clTable=angular.module("cl.table",[]);clTable.directive("clTable",function(){return{templateUrl:"clTable.html",restrict:"E",scope:{options:"=options"},link:function(t,e,r){t.searchItem={},t.sortType=t.options.contentAttributes[0],t.sortReverse=!1,t.sort=function(e){t.sortType=t.options.contentAttributes[e],t.sortReverse=!t.sortReverse},t.isObject=function(t){return angular.isObject(t)}}}});
+/*!
+ * CLAngularJSclTable
+ * 
+ * Version: 1.0.0 - 2016-02-18T18:45:01.351Z
+ * License: MIT
+ */
+
+
+(function () { 
+"use strict";
+var clTable = angular.module('cl.table', []);
+
+clTable.directive('clTable', function() {
+  return {
+    templateUrl: 'clTable.html',
+    restrict: 'E',
+    scope: {
+      options: '=options'
+    },
+    link: function(scope, element, attrs) {
+      scope.searchItem = {};
+      scope.sortType = scope.options.contentAttributes[0];
+      scope.sortReverse = false;
+      scope.sort = function (index) {
+        scope.sortType = scope.options.contentAttributes[index];
+        scope.sortReverse = !scope.sortReverse;
+      };
+      scope.isObject = function (input) {
+        return angular.isObject(input);
+      };
+    }
+  };
+});
+
+}());
+angular.module("cl.table").run(["$templateCache", function($templateCache) {$templateCache.put("clTable.html","<div ng-if=\"options.searchable\"><div class=\"form-group\" ng-repeat=\"item in options.contentAttributes\"><label>{{options.heading[$index]}}</label> <input type=\"text\" class=\"form-control\" ng-model=\"searchItem[item]\"></div></div><table class=\"table\"><tr><th ng-repeat=\"headline in options.heading\" ng-click=\"sort($index)\">{{headline}}</th></tr><tr ng-repeat=\"item in options.content | orderBy:sortType:sortReverse | filter:searchItem\"><td ng-repeat=\"attribute in options.contentAttributes\"><span ng-if=\"!isObject(item[attribute])\">{{item[attribute]}}</span> <button ng-if=\"isObject(item[attribute])\" ng-click=\"item[attribute].fn(item)\" class=\"btn btn-sm btn-primary\">{{item[attribute].display}}</button></td></tr></table>");}]);
