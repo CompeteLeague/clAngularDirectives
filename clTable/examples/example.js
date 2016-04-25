bc9d8cd79f6900729ff45965bc1bd6ef73d46369
youchra <nemmo@gmx.de>
2016-04-25 16:33:11 +0200

    cl table now supporting multiselect with generic functions

--- clTable/examples/example.js
+++ clTable/examples/example.js
@@ -43,4 +43,55 @@ module.controller('ExampleCtrl', ['$scope', function($scope) {
     contentAttributes: ['name', 'region', 'tournamenttype', 'date', 'action'],
     searchable: true
   };
+  $scope.tableOptionsMultiselect = {
+    content: [{
+      name: 'Bronze/Silver Season 1',
+      region: 'NA',
+      tournamenttype: 'Season Open',
+      date: '12.09.2015',
+      action: {
+        display: 'Print It',
+        fn: function () {
+          console.log('Hello world!');
+        },
+        class: 'btn-primary'
+      }
+    }, {
+      name: 'Gold/Platinum Season 1',
+      region: 'NA',
+      tournamenttype: 'Season Open',
+      date: '12.09.2015',
+      action: {
+        display: 'Print It',
+        fn: function () {
+          console.log('Hello world!');
+        },
+        class: 'btn-danger'
+      }
+    }, {
+      name: 'Diamon/Above Season 1',
+      region: 'NA',
+      tournamenttype: 'Season Open',
+      date: '12.09.2015',
+      action: {
+        display: 'Print It',
+        fn: function () {
+          console.log('Hello world!');
+        },
+        class: 'btn-warning'
+      }
+    }],
+    heading: ['Name', 'Region', 'Type', 'Date', 'Action'],
+    contentAttributes: ['name', 'region', 'tournamenttype', 'date', 'action'],
+    searchable: true,
+    multiselect: true,
+    multiselectfunction: {
+      name: 'My Super Duper Name',
+      fn: function (items) {
+        for (var i = 0; i < items.length; i++) {
+          console.log(items[i]);
+        }
+      }
+    }
+  };
 }]);
--- clTable/src/clTable.js
+++ clTable/src/clTable.js
@@ -11,6 +11,26 @@ clTable.directive('clTable', function() {
       scope.searchItem = {};
       scope.sortType = scope.options.contentAttributes[0];
       scope.sortReverse = false;
+      scope.selectedItems = [];
+      scope.selectedItemsIndizes = [];
+      scope.updateSelection = function (index, item, $event) {
+        var checkbox = $event.target;
+        var action = (checkbox.checked ? 'add' : 'remove');
+        updateSelected(action, index, item);
+      };
+      function updateSelected(action, index, item) {
+        if (action === 'add' && scope.selectedItemsIndizes.indexOf(index) === -1) {
+         scope.selectedItems.push(item);
+         scope.selectedItemsIndizes.push(index);
+         }
+        if (action === 'remove' && scope.selectedItemsIndizes.indexOf(index) !== -1) {
+         scope.selectedItems.splice(scope.selectedItemsIndizes.indexOf(index), 1);
+         scope.selectedItemsIndizes.splice(scope.selectedItemsIndizes.indexOf(index), 1);
+        }
+      }
+      scope.isSelected = function(index) {
+        return scope.selectedItemsIndizes.indexOf(index) >= 0;
+      };
       scope.sort = function (index) {
         scope.sortType = scope.options.contentAttributes[index];
         scope.sortReverse = !scope.sortReverse;
